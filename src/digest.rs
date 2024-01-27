14e818f47a257134f728296b8e2d7c0f22b8e3cf
Johannes Røsvik <j.rosvik@gmail.com>
2024-01-27 18:57:51 +0100

    update docs

--- src/digest.rs
+++ src/digest.rs
@@ -1,36 +1,9 @@
 use sha2::{Digest, Sha256};
 
-/*
-https://github.com/opencontainers/image-spec/blob/v1.0.1/descriptor.md#digests
-
-The digest property of a Descriptor acts as a content identifier, enabling
-content addressability. It uniquely identifies content by taking a collision-
-resistant hash of the bytes. If the digest can be communicated in a secure
-manner, one can verify content from an insecure source by recalculating the
-digest independently, ensuring the content has not been modified.
-
-Example:
-sha256:6c3c624b58dbbcd3c0dd82b4c53f04194d1247c6eebdaab7c610cf7d66709b3b
-
-A digest is calculated by the following pseudo-code, where H is the selected
-hash algorithm, identified by string <alg>:
-
-```
-let ID(C) = Descriptor.digest
-let C = <bytes>
-let D = '<alg>:' + Encode(H(C))
-let verified = ID(C) == D
-```
-
-Above, we define the content identifier as ID(C), extracted from the
-Descriptor.digest field. Content C is a string of bytes. Function H returns the
-hash of C in bytes and is passed to function Encode and prefixed with the
-algorithm to obtain the digest. The result verified is true if ID(C) is equal to
-D, confirming that C is the content identified by D.
-
-After verification, the following is true:
-D == ID(C) == '<alg>:' + Encode(H(C))
-*/
+// https://github.com/opencontainers/image-spec/blob/v1.0.1/descriptor.md#digests
+//
+// Where the client provided digest is C, and client provided data is D:
+// C == get_sha256_digest(D) == 'sha256:' + bytes_to_hex_string(hash_data(D))
 
 pub fn get_sha256_digest(data: &Vec<u8>) -> String {
   let hash_bytes = hash_data(data);
