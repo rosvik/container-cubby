0815ee400b0f8fa40f427b3ea4acbcea4f986d9f
Johannes Røsvik <j.rosvik@gmail.com>
2024-02-03 22:47:48 +0100

    add post_then_put test

--- src/main.rs
+++ src/main.rs
@@ -324,6 +324,49 @@ mod tests {
   }
 
   #[tokio::test]
+  async fn test_post_then_put() {
+    let namespace: String = get_random_namespace();
+    let test_blob: Bytes = Bytes::from("test_post_then_put");
+    db::init().unwrap();
+
+    // POST to get reference
+    let response = post_blob(
+      Path(namespace.to_string()),
+      Query(PostBlobParameters { digest: None }),
+      Bytes::new(),
+    )
+    .await
+    .into_response();
+    let location = response.headers().get("Location").unwrap();
+    let reference = location.to_str().unwrap().split('/').last().unwrap();
+    assert_eq!(response.status(), StatusCode::ACCEPTED);
+    assert!(!reference.is_empty());
+
+    // PUT entire blob
+    let digest = get_sha256_digest(&test_blob.to_vec());
+    let mut headers = HeaderMap::new();
+    headers.insert(
+      "Content-Length",
+      HeaderValue::from_str(test_blob.len().to_string().as_str()).unwrap(),
+    );
+    let put_response = put_blob(
+      Path((namespace.to_string(), reference.to_string())),
+      Query(PutBlobParameters {
+        digest: digest.clone(),
+      }),
+      headers,
+      test_blob,
+    )
+    .await
+    .into_response();
+    assert_eq!(put_response.status(), StatusCode::CREATED);
+
+    // Verify that the blob can be retrieved
+    let blob = get_blob(Path((namespace.to_string(), digest))).await.into_response();
+    assert_eq!(blob.status(), StatusCode::OK);
+  }
+
+  #[tokio::test]
   async fn test_get_blob() {
     let namespace = get_random_namespace();
     let test_blob_string: &str = "testblob";
