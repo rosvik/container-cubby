a9eeb0070dea2b1372153de05380cb07b797eb6c
Johannes Røsvik <j.rosvik@gmail.com>
2024-08-26 18:00:56 +0200

    add test_post_then_put

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -34,3 +34,44 @@ async fn test_create_blob() {
   let location = res.headers().get("Location").unwrap();
   assert!(location.to_str().unwrap().contains(name.as_str()));
 }
+
+#[test]
+async fn test_post_then_put() {
+  let _ = db::init();
+
+  let name: String = get_random_namespace();
+  let blob = "testblob".as_bytes();
+  let digest = digestor::get_sha256_digest(&blob.to_vec());
+
+  let app = App::new()
+    .service(web::resource("/v2/{name}/blobs/uploads/").post(post_blob_upload))
+    .service(web::resource("/v2/{name}/blobs/uploads/{reference}").put(put_blob_upload));
+  let service = test::init_service(app).await;
+
+  // POST to get reference
+  let uri = format!("/v2/{}/blobs/uploads/", name);
+  let req = test::TestRequest::with_uri(uri.as_str())
+    .insert_header(("Content-Length", blob.len().to_string()))
+    .insert_header(("Content-Type", "application/octet-stream"))
+    .insert_header(("Content-Range", "0-9"))
+    .method(Method::POST)
+    .to_request();
+
+  let res = service.call(req).await.unwrap();
+  assert_eq!(res.status(), StatusCode::ACCEPTED);
+
+  let location = res.headers().get("Location").unwrap();
+  let reference = location.to_str().unwrap().split('/').last().unwrap();
+
+  // PUT entire blob
+  let uri = format!("/v2/{}/blobs/uploads/{}?digest={}", name, reference, digest);
+  let req = test::TestRequest::with_uri(uri.as_str())
+    .set_payload(blob)
+    .insert_header(("Content-Length", blob.len().to_string()))
+    .insert_header(("Content-Type", "application/octet-stream"))
+    .insert_header(("Content-Range", "0-9"))
+    .method(Method::PUT)
+    .to_request();
+  let res = service.call(req).await.unwrap();
+  assert_eq!(res.status(), StatusCode::CREATED);
+}
