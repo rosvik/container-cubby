a8a17b0d3a38496fb144329d2587c9836f420d95
Johannes Røsvik <j.rosvik@gmail.com>
2024-08-26 14:58:32 +0200

    implement LogRequests middleware

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,12 +1,16 @@
 use crate::utils;
-use actix_web::dev::ServiceRequest;
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
 use axum::{
   extract::Request,
   http::{HeaderMap, HeaderValue, StatusCode},
   middleware::Next,
   response::Response,
 };
-use std::env;
+use futures_util::future::LocalBoxFuture;
+use std::{
+  env,
+  future::{ready, Ready},
+};
 
 pub fn log_requests(request: &ServiceRequest) {
   println!("\n\x1b[1;34m{} {:?}\x1b[0m", &request.method(), &request.uri());
@@ -59,5 +63,65 @@ pub async fn basic_authenticate(headers: HeaderMap, request: Request, next: Next
 
       next.run(request).await
     }
+  }
+}
+
+pub struct LogRequests;
+// `S` - type of the next service
+// `B` - type of response's body
+impl<S, B> Transform<S, ServiceRequest> for LogRequests
+where
+  S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = actix_web::Error>,
+  S::Future: 'static,
+  B: 'static,
+{
+  type Response = ServiceResponse<B>;
+  type Error = actix_web::Error;
+  type InitError = ();
+  type Transform = LogRequestsMiddleware<S>;
+  type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+  fn new_transform(&self, service: S) -> Self::Future {
+    ready(Ok(LogRequestsMiddleware { service }))
+  }
+}
+
+pub struct LogRequestsMiddleware<S> {
+  service: S,
+}
+impl<S, B> Service<ServiceRequest> for LogRequestsMiddleware<S>
+where
+  S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = actix_web::Error>,
+  S::Future: 'static,
+  B: 'static,
+{
+  type Response = ServiceResponse<B>;
+  type Error = actix_web::Error;
+  type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+  fn call(&self, req: ServiceRequest) -> Self::Future {
+    // Request
+    println!("\n\x1b[1;35mRequest: \x1b[1;34m{} {:?}\x1b[0m", &req.method(), &req.uri());
+    req.headers().iter().for_each(|(name, value)| {
+      println!("  \x1b[36m{}: {:?}\x1b[0m", name, value);
+    });
+
+    // Response
+    let fut = self.service.call(req);
+    Box::pin(async move {
+      let res = fut.await?;
+      println!("\x1b[1;35mResponse: \x1b[34m{:?}\x1b[0m", res.status());
+      res.headers().iter().for_each(|(name, value)| {
+        println!("  \x1b[36m{}: {:?}\x1b[0m", name, value);
+      });
+      Ok(res)
+    })
+  }
+
+  fn poll_ready(
+    &self,
+    ctx: &mut core::task::Context<'_>,
+  ) -> std::task::Poll<Result<(), Self::Error>> {
+    self.service.poll_ready(ctx)
   }
 }
