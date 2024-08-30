d5fe582cc2ee6b6410fce528f13ad2929f788618
Johannes Røsvik <j.rosvik@gmail.com>
2024-08-30 23:08:11 +0200

    refactor filesystem.rs

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,108 +1,72 @@
-use regex_lite::Regex;
+use crate::utils;
 use std::fs::{DirBuilder, File};
 use std::io;
 
 const DATA_DIR: &str = "data";
 
-fn make_blobs_dir(folder: &str, digest: &str) -> Result<String, io::Error> {
+fn prepare_blob(digest: &str) -> Result<(String, String), io::Error> {
+  if !utils::is_safe_digest(digest) {
+    return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Unsafe digest: {}", digest)));
+  }
+
   let digest = digest.replace("sha256:", "");
   let prefix = digest.chars().take(2).collect::<String>();
-  let directory = format!("{DATA_DIR}/{folder}/{prefix}/");
+  let directory = format!("{DATA_DIR}/blobs/{prefix}/");
   DirBuilder::new().recursive(true).create(&directory)?;
-  Ok(directory)
+
+  let rest = digest.chars().skip(2).collect::<String>();
+  let file_name = format!("{rest}.blob");
+
+  Ok((directory, file_name))
 }
 
-fn make_named_dir(folder: &str, name: &str) -> Result<String, io::Error> {
-  let directory = format!("{DATA_DIR}/{folder}/{name}/");
+fn prepare_manifest(name: &str, reference: &str) -> Result<(String, String), io::Error> {
+  if !utils::is_safe_name(name) {
+    return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Unsafe name: {}", name)));
+  }
+  if !utils::is_safe_reference(reference) {
+    return Err(io::Error::new(
+      io::ErrorKind::InvalidInput,
+      format!("Unsafe reference: {}", reference),
+    ));
+  }
+
+  let directory = format!("{DATA_DIR}/manifests/{name}/");
   DirBuilder::new().recursive(true).create(&directory)?;
-  Ok(directory)
-}
 
-fn get_blob_file_name(digest: &str, extension: &str) -> String {
-  let digest = digest.replace("sha256:", "");
-  let digest = format!("{}.{extension}", digest);
-  digest.chars().skip(2).collect::<String>()
-}
-
-fn get_manifest_file_name(digest: &str, extension: &str) -> String {
-  match digest.starts_with("sha256:") {
+  let file_name = match reference.starts_with("sha256:") {
     true => {
-      let digest = digest.replace("sha256:", "");
-      format!("{}.{extension}", digest)
+      let digest = reference.replace("sha256:", "");
+      format!("{}.json", digest)
     }
     false => {
-      format!("{}.{extension}", digest)
+      format!("{}.json", reference)
     }
-  }
+  };
+
+  Ok((directory, file_name))
 }
 
 pub fn create_blob_file(digest: &str) -> Result<File, io::Error> {
-  let directory = make_blobs_dir("blobs", digest)?;
-  let file_name = get_blob_file_name(digest, "blob");
-
+  let (directory, file_name) = prepare_blob(digest)?;
   let file = File::create(format!("{directory}/{file_name}"))?;
   Ok(file)
 }
 
 pub fn get_blob_file(digest: &str) -> Result<File, io::Error> {
-  let directory = make_blobs_dir("blobs", digest)?;
-  let file_name = get_blob_file_name(digest, "blob");
-
+  let (directory, file_name) = prepare_blob(digest)?;
   let file = File::open(format!("{directory}/{file_name}"))?;
   Ok(file)
 }
 
-/// Create a manifest file
-///
-/// - name: &str - Namespace. MUST match the following regular expression:
-///   `[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*(\/[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*)*`
-///
-/// - reference: &str - Reference as a tag MUST be at most 128 characters in
-///   length and MUST match the following regular expression:
-///   `[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}`
 pub fn create_manifest_file(name: &str, reference: &str) -> Result<File, io::Error> {
-  if !is_safe_name(name) {
-    return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Unsafe name: {}", name)));
-  }
-  if !is_safe_reference(reference) {
-    return Err(io::Error::new(
-      io::ErrorKind::InvalidInput,
-      format!("Unsafe reference: {}", reference),
-    ));
-  }
-
-  let directory = make_named_dir("manifests", name)?;
-  let file_name = get_manifest_file_name(reference, "json");
-
+  let (directory, file_name) = prepare_manifest(name, reference)?;
   let file = File::create(format!("{directory}/{file_name}"))?;
   Ok(file)
 }
 
 pub fn get_manifest_file(name: &str, reference: &str) -> Result<File, io::Error> {
-  if !is_safe_name(name) {
-    return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Unsafe name: {}", name)));
-  }
-  if !is_safe_reference(reference) {
-    return Err(io::Error::new(
-      io::ErrorKind::InvalidInput,
-      format!("Unsafe reference: {}", reference),
-    ));
-  }
-
-  let directory = make_named_dir("manifests", name)?;
-  let file_name = get_manifest_file_name(reference, "json");
-
+  let (directory, file_name) = prepare_manifest(name, reference)?;
   let file = File::open(format!("{directory}/{file_name}"))?;
   Ok(file)
-}
-
-pub fn is_safe_name(path: &str) -> bool {
-  let re = Regex::new(r"^[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*(\/[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*)*$")
-    .unwrap();
-  re.is_match(path)
-}
-
-pub fn is_safe_reference(reference: &str) -> bool {
-  let re = Regex::new(r"^[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}$").unwrap();
-  re.is_match(reference)
 }
