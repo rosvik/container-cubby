a035211dbca0bdd04fde8030866c958ec5feb249
Johannes Røsvik <j.rosvik@gmail.com>
2024-09-01 19:06:23 +0200

    print test namespaces

--- src/tests/utils.rs
+++ src/tests/utils.rs
@@ -3,5 +3,7 @@ use std::iter::repeat_with;
 
 pub fn get_random_namespace() -> String {
   let random_string: String = repeat_with(fastrand::lowercase).take(10).collect();
-  format!("test-{CRATE_VERSION}-{}", random_string)
+  let namespace = format!("test-{CRATE_VERSION}-{}", random_string);
+  println!("Using namespace: {}", namespace);
+  namespace
 }
