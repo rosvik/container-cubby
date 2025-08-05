2792510c9d642aeb2f5c4c8106af1c8785362e7f
Johannes Røsvik <j.rosvik@gmail.com>
2025-08-05 17:05:28 +0200

    feat: add pruning of dangling manifests

--- src/storage/prune.rs
+++ src/storage/prune.rs
@@ -7,14 +7,11 @@ Prune modes:
 Running `container-cubby --prune untagged` could
 1. delete all manifests that are not linked to by a tag
 2. delete all blob links that are not linked to by a manifest
-3. delete all blobs that are not linked to by a blob link
+3. delete all blobs that are not linked to by a blob link ✅
 */
 
-use crate::{
-  env,
-  storage::{file, path},
-};
-use std::fs;
+use crate::{env, storage::file};
+use std::{fmt, fs, path::Path};
 
 pub enum PruneMode {
   /// If no symlinks in the container directory links to a blob, delete the blob.
@@ -24,41 +21,89 @@ pub enum PruneMode {
   /// If a blob link is not referenced by a manifest, delete the blob link.
   BlobLinks,
 }
-
-#[allow(dead_code)]
-pub fn prune(mode: PruneMode, dry_run: bool) {
-  match mode {
-    PruneMode::DanglingBlobs => prune_dangling_blobs(dry_run),
-    PruneMode::DanglingManifests => prune_dangling_manifests(dry_run),
-    PruneMode::BlobLinks => prune_blob_links(dry_run),
+impl fmt::Display for PruneMode {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      PruneMode::DanglingBlobs => write!(f, "dangling blobs"),
+      PruneMode::DanglingManifests => write!(f, "dangling manifests"),
+      PruneMode::BlobLinks => write!(f, "blob links"),
+    }
   }
 }
 
-fn prune_dangling_blobs(dry_run: bool) {
+#[allow(dead_code)]
+pub fn prune(mode: PruneMode, dry_run: bool) {
+  println!("Pruning {mode} from database");
+
+  let result = match mode {
+    PruneMode::DanglingBlobs => prune_dangling_blobs(),
+    PruneMode::DanglingManifests => get_dangling_manifests(),
+    PruneMode::BlobLinks => get_blob_links(),
+  };
+  if result.is_empty() {
+    println!("No {mode} to delete");
+    return;
+  }
+  if !dry_run {
+    println!("Deleting {} files:", result.len());
+    for item in result {
+      println!("{item}");
+      file::delete(&item).unwrap();
+    }
+  } else {
+    println!("Would delete {} files:", result.len());
+    for item in result {
+      println!("{item}");
+    }
+  }
+}
+
+fn prune_dangling_blobs() -> Vec<String> {
   let all_blob_shas = list_all_blob_shas();
   let all_blob_link_targets = list_all_blob_link_target_shas();
 
   // Find all blobs that are not in all_blob_link_targets
-  let dangling_blobs = all_blob_shas
+  all_blob_shas
     .iter()
     .filter(|sha| !all_blob_link_targets.contains(sha))
-    .collect::<Vec<&String>>();
+    .map(|s| s.to_string())
+    .collect::<Vec<String>>()
+}
 
-  println!("Found {} dangling blobs", dangling_blobs.len());
-  for blob in dangling_blobs {
-    let blob_path = path::get("", blob, path::FileType::Blob).unwrap();
-    if !dry_run {
-      println!("Deleting dangling blob: {blob_path}");
-      file::delete(&blob_path).unwrap();
-    } else {
-      println!("(dry-run) Would delete dangling blob: {blob_path}");
-    }
-  }
+fn get_dangling_manifests() -> Vec<String> {
+  let containers_dir = format!("{}/containers", env::data_dir());
+  let manifests = recursively_find(&containers_dir, |path| {
+    path.ends_with(".json") && path.split("/").last().unwrap().starts_with("sha256:")
+  });
+
+  // Search through the directory of each manifest to find a Tag that targets the manifest
+  manifests
+    .iter()
+    .filter(|manifest| {
+      let manifest_path = Path::new(manifest);
+      let manifest_file_name = manifest_path.file_name().unwrap().to_str().unwrap();
+      let manifest_dir = fs::read_dir(manifest_path.parent().unwrap()).unwrap();
+      for file in manifest_dir {
+        let file = file.unwrap();
+
+        // Is the file a Tag?
+        if file.path().is_symlink() && file.path().to_str().unwrap().ends_with(".json") {
+          let symlink_target = fs::read_link(file.path()).unwrap();
+
+          // Does the Tag target the manifest?
+          if symlink_target.to_str().unwrap() == manifest_file_name {
+            // The manifest is linked to by a tag, so it is not dangling
+            return false;
+          }
+        }
+      }
+      // The manifest is not linked to by a tag, so it is dangling
+      true
+    })
+    .map(|s| s.to_string())
+    .collect::<Vec<String>>()
 }
-fn prune_dangling_manifests(_dry_run: bool) {
-  panic!("Not implemented");
-}
-fn prune_blob_links(_dry_run: bool) {
+fn get_blob_links() -> Vec<String> {
   panic!("Not implemented");
 }
 
@@ -81,19 +126,19 @@ fn list_all_blob_shas() -> Vec<String> {
 }
 
 fn list_all_blob_link_target_shas() -> Vec<String> {
-  let blob_dir = format!("{}/containers", env::data_dir());
-  let target_paths = recursively_find(&blob_dir, ".blob");
+  let containers_dir = format!("{}/containers", env::data_dir());
+  let target_paths = recursively_find(&containers_dir, |path| path.ends_with(".blob"));
   target_paths.iter().map(|path| path.replace(".blob", "")).collect::<Vec<String>>()
 }
 
-fn recursively_find(dir_path: &str, ends_with: &str) -> Vec<String> {
+fn recursively_find(dir_path: &str, predicate: impl Fn(&str) -> bool + Clone) -> Vec<String> {
   let dir = fs::read_dir(dir_path).unwrap();
   let mut matches = Vec::new();
   for entry in dir {
     let path = entry.unwrap().path();
     if path.is_dir() {
-      matches.extend(recursively_find(path.to_str().unwrap(), ends_with));
-    } else if path.is_file() && path.file_name().unwrap().to_str().unwrap().ends_with(ends_with) {
+      matches.extend(recursively_find(path.to_str().unwrap(), predicate.clone()));
+    } else if path.is_file() && predicate(path.to_str().unwrap()) {
       matches.push(path.to_str().unwrap().to_string());
     }
   }
@@ -106,6 +151,7 @@ mod tests {
 
   #[test]
   fn test_prune_dangling_blobs() {
-    prune(PruneMode::DanglingBlobs, false);
+    prune(PruneMode::DanglingManifests, true);
+    // prune(PruneMode::DanglingBlobs, true);
   }
 }
